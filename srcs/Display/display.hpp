f527dcf5b83e79ddd08ccaa84242bda7fcbe56d2
Guillaume Aubert <guillaume@aubert.ws>
2022-11-28 14:49:07 +0100

    Better file formatting and removing colors.hpp

--- srcs/Display/display.hpp
+++ srcs/Display/display.hpp
@@ -4,23 +4,23 @@
 #include <string>
 #include <map>
 
-class Display {
-    public:
-        Display(std::map<std::string, std::string> *pChanPtr, std::map<std::string, std::string> *pUsrPtr);
-        Display();
-        ~Display();
-        void    update();
-        void    addMessage(std::string msg);
-        std::string    color(uint8_t r, uint8_t g, uint8_t b);
-        std::string    color();
+class Display
+{
+public:
+	Display(std::map<std::string, std::string> *pChanPtr, std::map<std::string, std::string> *pUsrPtr);
+	Display();
+	~Display();
+	void update();
+	void addMessage(std::string msg);
+	std::string color(uint8_t r, uint8_t g, uint8_t b);
+	std::string color();
 
-    private:
-        bool _fancyMode;
-        std::string    trunc(std::string str, int len);
-        std::vector<std::string> _messages;
-        std::map<std::string, std::string> *_users;
-        std::map<std::string, std::string> *_channels;
-
+private:
+	bool _fancyMode;
+	std::string trunc(std::string str, int len);
+	std::vector<std::string> _messages;
+	std::map<std::string, std::string> *_users;
+	std::map<std::string, std::string> *_channels;
 };
 
 #endif
