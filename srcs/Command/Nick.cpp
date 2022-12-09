66d95286c5f8ddd4a8b84cdc0f24e88dd91a0358
gaubert <gaubert@student.42lausanne.ch>
2022-12-09 16:40:58 +0100

    better nick answers

--- srcs/Command/Nick.cpp
+++ srcs/Command/Nick.cpp
@@ -14,9 +14,9 @@ Nick::~Nick()
 int	Nick::execute(User &user, std::vector<std::string> args)
 {
 	if (args.size() < 2)
-		user.sendMsg(":No nickname given\r\n");
+		user.sendMsg(":" + user.getHost() + "431" + ":No nickname given\r\n");
 	else if (_serv->findFdByNickname(args[1]) != 0)
-		user.sendMsg(args[1] + " :Nickname is already in use\r\n");
+		user.sendMsg(":" + user.getHost() + "433" + args[1] + " :Nickname is already in use\r\n");
 	else
 	{
 		std::string oldNick = user.getNickname();
